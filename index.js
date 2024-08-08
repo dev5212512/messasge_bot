c3bb0fe1b554d4002bbe71604bbf459b56a09bdd
dev5212512 <dev5212512@gmail.com>
2024-08-08 22:29:44 +0500

    Initial application

---
+++ index.js
@@ -0,0 +1,36 @@
+const welcomeChannelId='random2'
+require("dotenv").config();
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true, // add this
+  appToken: process.env.SLACK_APP_TOKEN, // add this
+  logLevel: 'debug',
+  scopes: ['channels:history', 'chat:write', 'commands']
+});
+
+
+// Not working
+app.message('hello', async ({ message, say }) => {
+  console.log('=-----------------------------in---------------------')
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
+
+app.event('channel_rename', async({event, client, logger}) => {
+  await client.chat.postMessage({
+    channel: welcomeChannelId,
+    text: JSON.stringify(event)
+  })
+})
+
+
+(async () => {
+  // Start the app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
